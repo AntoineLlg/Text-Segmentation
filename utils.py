174a1becfa85bd957f835353fe9ee6393211dbe2
taucmar <antoinelelong.pro@gmail.com>
2022-10-03 22:46:11 +0200

    fonctions pour segmenter les lignes et les mots d'une image correctement orientée

--- utils.py
+++ utils.py
@@ -118,3 +118,57 @@ def find_skew_angle(im):
             coef.append(reg.coef_)
 
     return np.arctan(np.mean(coef))
+
+
+def cut_lines(im, s=0.95):
+    """
+    Crée un générateur qui renvoie dans l'ordre les indice de départ et fin des lignes de texte dans l'image.
+
+    :param im: 2D array
+    :param s: float dans (0, 1) le seuil en dessous duquel on considère qu'il y a de l'écriture dans la ligne
+    :return: generator
+    """
+    white_density = im.sum(axis=-1)
+    threshold = s * white_density.max()
+
+    text_line = white_density < threshold
+    init = False
+    for i in range(len(white_density)):
+        if init:
+            if text_line[i]:
+                end = i
+            else:
+                init = False
+                yield start, end + 1
+        else:
+            if text_line[i]:
+                start = i
+                end = i
+                init = True
+
+
+def cut_words(line, s=0.98):
+    """
+    Crée un générateur qui renvoie dans l'ordre les indice de départ et fin des mots de texte dans la ligne.
+
+    :param line: 2D array
+    :param s: float dans (0, 1) le seuil en dessous duquel on considère qu'il y a de l'écriture dans la ligne
+    :return: generator
+    """
+    white_density = line.sum(axis=0)
+    threshold = s * white_density.max()
+
+    text_line = white_density < threshold
+    init = False
+    for i in range(len(white_density)):
+        if init:
+            if text_line[i]:
+                end = i
+            else:
+                init = False
+                yield start, end + 1
+        else:
+            if text_line[i]:
+                start = i
+                end = i
+                init = True
